f1878d31a7f67380b7f0604e41cff58756847ab6
Firasama29 <67781796+Firasama29@users.noreply.github.com>
2021-01-05 09:25:36 +0800

    student dto

--- src/main/java/com/students/model/Student.java
+++ src/main/java/com/students/model/Student.java
@@ -5,13 +5,11 @@ import java.io.Serializable;
 public class StudentDTO implements Serializable{
 
 	private static final long serialVersionUID = -5025162008856120028L;
+	
 	private long id;
-	private String firstName;
-	private String surName;
+	private String fullName;
 	private String email;
 	private String password;
-	private String course;
-	private int age;
 	
 	
 	public long getId() {
@@ -20,17 +18,11 @@ public class StudentDTO implements Serializable{
 	public void setId(long id) {
 		this.id = id;
 	}
-	public String getFirstName() {
-		return firstName;
+	public String getFullName() {
+		return fullName;
 	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getSurName() {
-		return surName;
-	}
-	public void setSurName(String surName) {
-		this.surName = surName;
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
 	}
 	public String getEmail() {
 		return email;
@@ -43,17 +35,5 @@ public class StudentDTO implements Serializable{
 	}
 	public void setPassword(String password) {
 		this.password = password;
-	}
-	public String getCourse() {
-		return course;
-	}
-	public void setCourse(String course) {
-		this.course = course;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
 	}
 }
